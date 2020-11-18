c62290510f8f9f04bddb247882c439b328a041ec
Jinhyo <sosilion@gmail.com>
2020-11-18 17:17:32 +0900

    1. base setting

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/:id" component={Detail} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
