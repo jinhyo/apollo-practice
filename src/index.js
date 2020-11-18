bc6b63de3eb7af715a0481a7a3a8893314bece1d
Jinhyo <sosilion@gmail.com>
2020-11-18 17:55:14 +0900

    2. apollo setting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import client from "./apollo";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
